5eac2b84b66637d8dce20fbeb38fcedd160c619b
cdmstrong <1283482593@qq.com>
2023-04-20 17:26:13 +0800

    init

---
+++ main.py
@@ -0,0 +1,65 @@
+# Import TF and TF Hub libraries.
+import tensorflow as tf
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plot
+from demo import *
+from model import *
+def show_res(img, results, path_name = "out.png"):
+    for _, item in enumerate(results):
+        cv.circle(img, (int(item[1]), int(item[0])), 5, (0, 255, 0), 3)
+    cv.imwrite(path_name, img)
+# Load the input image.
+def pre_process(image_path):
+    if isinstance(image_path, str):
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_jpeg(image)
+    else:
+        image = image_path
+    input_size = 256
+    input_image = tf.expand_dims(image, axis=0)
+    input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
+    return input_image, image
+image_path = './imgs/input_image.jpeg'
+
+video_path = "./imgs/fit.mp4"
+cap = cv.VideoCapture(video_path)
+while True:
+    ret, img = cap.read()
+    if ret == None:
+        break
+    img = tf.convert_to_tensor(img)
+    input_image, ori = pre_process(img)
+    keypoints_with_scores = movenet(input_image)
+
+    # Visualize the predictions with image.
+    display_image = tf.expand_dims(ori, axis=0)
+    display_image = tf.cast(tf.image.resize_with_pad(
+        display_image, 640, 640), dtype=tf.int32)
+    output_overlay = draw_prediction_on_image(
+        np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+    # plt.figure(figsize=(5, 5))
+    # plt.imshow(output_overlay)
+    cv.imshow('h', cv.cvtColor(output_overlay, cv.COLOR_BGR2RGB))
+    if cv.waitKey(10) == ord("q"):
+        cv.destroyAllWindows()
+# input_image, ori_img = pre_process(image_path)
+
+# # Run model inference.
+# keypoints_with_scores = movenet(input_image)
+
+# # Visualize the predictions with image.
+# display_image = tf.expand_dims(ori_img, axis=0)
+# display_image = tf.cast(tf.image.resize_with_pad(
+#     display_image, 1280, 1280), dtype=tf.int32)
+# output_overlay = draw_prediction_on_image(
+#     np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+# # plt.figure(figsize=(5, 5))
+# plt.imshow(output_overlay)
+# cv.imshow('h', cv.cvtColor(output_overlay, cv.COLOR_BGR2RGB))
+# if cv.waitKey(0) == ord("q"):
+#     cv.destroyAllWindows()
+# _ = plt.axis('off')
+# plt.show()
---
+++ model.py
@@ -0,0 +1,17 @@
+# import wget
+import tensorflow as tf
+
+def movenet(input_image):
+    
+    interpreter = tf.lite.Interpreter(model_path="weights/lite-model_movenet_singlepose_thunder_tflite_float16_4.tflite")
+    interpreter.allocate_tensors()
+    # TF Lite format expects tensor type of uint8.
+    input_image = tf.cast(input_image, dtype=tf.uint8)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.set_tensor(input_details[0]['index'], input_image.numpy())
+    # Invoke inference.
+    interpreter.invoke()
+    # Get the model prediction.
+    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+    return keypoints_with_scores
